ca86b3f2da761d79ae9dc13c866c6f7fa7fa6aee
Yrjar V <yrjarvederhus@gmail.com>
2024-02-01 18:31:21 +0100

    Update to use GitHub secrets

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore"
 
 // Initalize Firebase
 export const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 }
 const app = initializeApp(firebaseConfig)
 const database = getFirestore(app)
